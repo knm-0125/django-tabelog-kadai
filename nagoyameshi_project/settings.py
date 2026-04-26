6b9a60c6e4d09c0bc55266d579297f0d7061097e
Konami <konami@example.com>
2026-04-26 20:25:47 +0900

    最終確認

--- nagoyameshi_project/settings.py
+++ nagoyameshi_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 import os
 
@@ -135,3 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "nagoyameshi/static",
+]
